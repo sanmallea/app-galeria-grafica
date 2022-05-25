2c974500393ccd6056fb1316f99c8380a4000dda
sanmallea <87240780+sanmallea@users.noreply.github.com>
2022-05-25 00:08:23 -0300

    Adding ItemCount final

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,13 +4,6 @@ import './Card.css';
 import Modal from '../Modal/Modal';
 import ItemCount from './ItemCount.js';
 
-//Todo lo que hay que hacer esta en el nuevo archivo ItemCount.js
-//Lo importante acá es justamente importarlo, está en la linea 25 acá, abajo de price
-//Tambien obvio llamarlo arriba en el import
-//Eso de Modal es algo que creo hicieron en la clase de apoyo, en la última clase hablaron muy poco de eso y la verdad
-//no lo agrego xq no creo agregue nada. Yo lo copie y está todo tal cual en el archivo Modal.js, pero no es importante
-
-
 const CardItem = ({ image, title, price, size }) => {
 
     return(
--- src/components/Card/ItemCount.js
+++ src/components/Card/ItemCount.js
@@ -3,21 +3,6 @@ import { Button } from '@mui/material';
 import './Card.css';
 import {useState} from 'react'
 
-//Lo primero que hay que hacer aca es agregar el div que tiene el count-item. Ese es el boton para agregar y sumar objetos
-
-//Fijate tambien que agregamos estilos de count-item en el css
-
-//Despues agrega el import useState, y agrega abajo el const que tiene setCount
-
-//Agrega el addCount y restCount en los botones y crea los const de cada uno. Esto es para
-//sumar y restar los numeros en la pagina
-
-//Agrega lo de Stock para justamente agregar el stock (jeje) El numero que esta en el useState seria el stock
-
-//Para terminar el ejercicio que piden, en el boton de comprar agrega un onClick y el console log para que 
-//aparezca cuantos comparias al hacer click en el boton
-
-
 const ItemCount = () => {
     //Sumar o restar productos
     const [count, setCount] = useState (1)
